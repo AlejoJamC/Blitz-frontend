f02d6ffbaca12ffcd8ffc635fbe3fc3ac067da4b
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-05-22 19:55:04 -0500

    auth routes post methods

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@
  * Module dependencies
  */
 var express = require('express');
+var request = require('request');
 var authRoutes = express.Router();
 var logger = require('../utils/logger').logger;
 
@@ -27,14 +28,87 @@ authRoutes.get('/login', function (req, res) {
      return res.redirect('/login');
      }
      */
-    // User Rol
-    // If ............
+
     res.render('auth/signin', {
         title: 'Iniciar sesión | Blitz',
         level: '../',
         isHome: true,
         layout: 'auth',
         error: error
+    });
+});
+
+/* POST Login page. */
+authRoutes.post('/login/ajax', function (req, res) {
+    // Basic error validator
+    var error = '';
+    // Error
+    if (typeof req.query.error !== 'undefined') {
+        error = req.query.error;
+    }
+    // Session
+    /*
+     if(typeof req.session.userId === 'undefined' || typeof req.session.userId === ''){
+     return res.redirect('/login');
+     }
+     */
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var api = process.env.API_URL + (process.env.API_PORT !== '' ? ':' + process.env.API_PORT : '')
+        + (process.env.API_VERSION !== '' ? '/' + process.env.API_VERSION : '');
+
+    var tokenCompose = email + ':' + password;
+    tokenCompose = new Buffer(tokenCompose).toString('base64');
+
+    logger.info(api + '/login');
+
+    // Request options
+    var options = {
+        url: api + '/users',
+        headers: {
+            'Authorization': 'Basic ' + tokenCompose,
+            'Content-Type': 'application/json'
+        },
+        method: 'GET'
+    };
+
+    request.get(options, function (err, httpResponse, body) {
+        if (httpResponse.statusCode !== 200) {
+            switch (httpResponse.statusCode) {
+                case 400:
+                    logger.error(err);
+                    return res.status(400).send({
+                        message: '	Invalid Request Body, could not be parsed as JSON | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                case 401:
+                    logger.error(err);
+                    return res.status(401).send({
+                        message: 'Unauthorized | Blitz Server',
+                        error: body
+                    });
+                    break;
+                case 422:
+                    logger.error(err);
+                    return res.status(422).send({
+                        message: 'Unprocessable Entity; there are errors in one or more of the fields provided in the request body | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                default:
+                    logger.error(err);
+                    return res.status(500).send({
+                        message: '	Server error - contact your administrator',
+                        error: body.errors
+                    });
+                    break;
+            }
+        }
+
+        return res.send(httpResponse);
     });
 });
 
@@ -52,14 +126,93 @@ authRoutes.get('/signup', function (req, res) {
      return res.redirect('/login');
      }
      */
-    // User Rol
-    // If ............
+
     res.render('auth/signup', {
         title: 'Iniciar sesión | Blitz',
         level: '../',
         layout: 'auth',
-        error: error,
-        apiUrl: process.env.API_URL + (process.env.API_PORT !== '' ? ':' + process.env.API_PORT : '') + (process.env.API_VERSION !== '' ? '/' + process.env.API_VERSION : '')
+        error: error
+    });
+});
+
+/* POST Signup page. */
+authRoutes.post('/signup/ajax', function (req, res) {
+    // Basic error validator
+    var error = '';
+    // Error
+    if (typeof req.query.error !== 'undefined') {
+        error = req.query.error;
+    }
+    // Session
+    /*
+     if(typeof req.session.userId === 'undefined' || typeof req.session.userId === ''){
+     return res.redirect('/login');
+     }
+     */
+
+    var firstname = req.body.firstName;
+    var lastname = req.body.lastName;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var api = process.env.API_URL + (process.env.API_PORT !== '' ? ':' + process.env.API_PORT : '')
+        + (process.env.API_VERSION !== '' ? '/' + process.env.API_VERSION : '');
+
+    var token = process.env.API_AUTH;
+
+    logger.info(api + '/users');
+
+    // Request options
+    var options = {
+        url: api + '/users',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        json:{
+            'firstName' : firstname,
+            'lastName' : lastname,
+            'email' : email,
+            'password' : password
+        }
+    };
+
+    request.post(options, function (err, httpResponse, body) {
+        if (httpResponse.statusCode !== 200) {
+            switch (httpResponse.statusCode) {
+                case 400:
+                    logger.error(err);
+                    return res.status(400).send({
+                        message: '	Invalid Request Body, could not be parsed as JSON | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                case 401:
+                    logger.error(err);
+                    return res.status(401).send({
+                        message: 'Unauthorized | Blitz Server',
+                        error: body
+                    });
+                    break;
+                case 422:
+                    logger.error(err);
+                    return res.status(422).send({
+                        message: 'Unprocessable Entity; there are errors in one or more of the fields provided in the request body | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                default:
+                    logger.error(err);
+                    return res.status(500).send({
+                        message: '	Server error - contact your administrator',
+                        error: body.errors
+                    });
+                    break;
+            }
+        }
+
+        return res.send(httpResponse);
     });
 });
 
@@ -77,8 +230,7 @@ authRoutes.get('/forgot', function (req, res) {
      return res.redirect('/login');
      }
      */
-    // User Rol
-    // If ............
+
     res.render('auth/forgot', {
         title: 'Recuperar contraseña | Blitz',
         level: '../',
@@ -86,6 +238,91 @@ authRoutes.get('/forgot', function (req, res) {
         error: error
     });
 });
+
+/* POST Forgot page. */
+authRoutes.post('/forgot/ajax', function (req, res) {
+    // Basic error validator
+    var error = '';
+    // Error
+    if (typeof req.query.error !== 'undefined') {
+        error = req.query.error;
+    }
+    // Session
+    /*
+     if(typeof req.session.userId === 'undefined' || typeof req.session.userId === ''){
+     return res.redirect('/login');
+     }
+     */
+
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var api = process.env.API_URL + (process.env.API_PORT !== '' ? ':' + process.env.API_PORT : '')
+        + (process.env.API_VERSION !== '' ? '/' + process.env.API_VERSION : '');
+
+    var token = process.env.API_AUTH;
+
+    logger.info(api + '/users');
+
+    // Request options
+    var options = {
+        url: api + '/users',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        json:{
+            'firstName' : firstname,
+            'lastName' : lastname,
+            'email' : email,
+            'password' : password
+        }
+    };
+
+    request.post(options, function (err, httpResponse, body) {
+        if (httpResponse.statusCode !== 200) {
+            switch (httpResponse.statusCode) {
+                case 400:
+                    logger.error(err);
+                    return res.status(400).send({
+                        message: '	Invalid Request Body, could not be parsed as JSON | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                case 401:
+                    logger.error(err);
+                    return res.status(401).send({
+                        message: 'Unauthorized | Blitz Server',
+                        error: body
+                    });
+                    break;
+                case 422:
+                    logger.error(err);
+                    return res.status(422).send({
+                        message: 'Unprocessable Entity; there are errors in one or more of the fields provided in the request body | Blitz Server',
+                        error: body.errors
+                    });
+                    break;
+                default:
+                    logger.error(err);
+                    return res.status(500).send({
+                        message: '	Server error - contact your administrator',
+                        error: body.errors
+                    });
+                    break;
+            }
+        }
+
+        logger.info(JSON.stringify(httpResponse));
+        logger.info(JSON.stringify(body));
+
+        return body;
+    });
+});
+
 
 /* GET Logout page. */
 authRoutes.get('/logout', function (req, res) {
