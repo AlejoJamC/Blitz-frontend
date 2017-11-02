f3818905264b3b7ce90a7a2cdb9697ae766ea527
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-11-02 03:08:13 -0500

    content html appended

--- public/js/content.js
+++ public/js/content.js
@@ -27,16 +27,29 @@
         var html = '';
 
         if (data.body !== 'undefined' && data.length > 0) {
+            console.log(data.length);
+            // html content
+            html = '<li class="grid-sizer"></li>';
+
+            console.log(data.length);
+            $.grep(data, function (murmur, index) {
+                console.log(murmur);
+                html += '<li class="b-isotope-grid__item grid-item design">' +
+                    '<a class="b-isotope-grid__inner" href="/content/murmur/' + murmur._id + '">' +
+                    '<img src="'+ '../' + murmur.image  + '" alt="foto" />' +
+                '<span class="b-isotope-grid__wrap-info">' +
+                '<span class="b-isotope-grid__info">' +
+                '<span class="b-isotope-grid__title">' + murmur.title + '</span>' +
+                '<span class="b-isotope-grid__categorie">' + murmur.type + '</span>' +
+                '</span></span></a></li>'
+            });
+
+            $('#murmurlist').append(html);
 
         } else {
             // add blank space
-            $contentlist.css({"margin-top":"100px"});
-            // html content
-            html = '<div class="ish-row">' +
-                '<span class="ish-col-xs-4 ish-col-sm-1 ish-sc-element ish-sc-icon ish-icon-large ish-txt-color1">' +
-                '<a href="javascript:;"><i class="ish-icon-hourglass"></i></a>' +
-                '</span><p>&nbsp;&nbsp;&nbsp;La lista de los soplos cardiacos no esta disponible.</p>' +
-                '</div>'
+            $contentlist.css({"margin-top": "100px"});
+
         }
 
         $contentlist.append(html);
