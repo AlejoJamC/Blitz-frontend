42d475a426e9902015b7bda2e1a89ba5c53f0b72
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-11-03 03:51:22 -0500

    admin index js functions

---
+++ public/js/adminindex.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2017-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+/**
+ * Sweetalerts
+ */
+(function ($) {
+    'use strict';
+
+    $( document ).ready(function() {
+        getCount();
+    });
+
+    function getCount() {
+        $.ajax({
+            method: "GET",
+            url: '/dashboard/entities/ajax'
+        }).done(function (data) {
+            var response = JSON.parse(data);
+            $('#adminCount').text(response.admins);
+            $('#userCount').text(response.murmurs);
+            $('#connCount').text(response.connections);
+            $('#murmurCount').text(response.murmurs);
+            $('#unmurmurCount').text(response.unmurmurs);
+            $('#clientCount').text(response.clients);
+        });
+    }
+
+})(jQuery);
